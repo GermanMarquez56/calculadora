e7021215fcb83eecf2d7b7aecab1323baa02eb75
germanmarquez56 <germanmarquez17@gmai.com>
2025-04-30 20:07:12 -0500

    clase 30abril

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from flask import Flask
+from flask import Flask,request,render_template
+from livereload import Server
+from operaciones import sumar,division_piso
+
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return '''
-        
-        aplicacion web con flask-afsr
-        <h1>calculadora</h1>
+    return render_template("home.html")
 
-'''
+
+
+@app.route("/suma")
+def ruta_suma():
+    numero1=request.args.get("numero1",type=float)
+    numero2=request.args.get("numero2",type=float)
+    if numero1 is None or numero2 is None:
+        return "faltan datos"
+    return f"el resultado de la suma es {sumar(numero1,numero2)}"
+
+
+@app.route("/division_piso")
+def ruta_division_piso():
+    numero1=request.args.get("numero1",type=float)
+    numero2=request.args.get("numero2",type=float)
+    if numero1 is None or numero2 is None:
+        return "faltan datos"
+    return f"el resultado de la suma es {sumar(numero1,numero2)}"
+
+
+if __name__ == "__main__":
+    server = Server(app.wsgi_app)
+    server.serve()
+
+
+
+
+
+
+
